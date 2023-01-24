1c76c7008427a4f69a48e2685877130bce572380
Loc@lTrash <105620888+Local-Trash@users.noreply.github.com>
2023-01-24 20:13:50 +0000

    some updates

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let binding = String::from_utf8(include_bytes!("input.txt").to_vec()).unwrap();
+    let input: Vec<_> = binding.split("\n").collect();
+
+    for x in &input {
+        let index = input.iter().position(|x| *x == " ");
+
+        println!("{}", index);
+    }
+
+    println!("{:?}", input);
 }
